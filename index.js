494be842704a3ded1adf5631e6aa7a05d5491bfc
Diana Orecchio <di.orecchio@gmail.com>
2017-09-12 13:52:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ if (name === "Susan") {
     console.log("Expected the name to be Susan")
 }
 
-if (height === "70")
-console.log("The height is correct")
+if (height === "70") {
+  console.log("The height is correct")
 } else {
   console.log("Expected the name to be 70")
 }
